5cf6f83e4ba1051a7cb5c74e7c5cb05038b01421
youkai <youkaisteve@gmail.com>
2017-08-13 09:18:44 +0800

    intergate swagger2

---
+++ src/main/java/com/sm/autosrv/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package com.sm.autosrv;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Created by youkai on 2017/8/13.
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.sm.autosrv.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Spring Boot中使用Swagger2构建RESTful APIs")
+                .description("请注意,这不是演戏,这……不是演戏!")
+                .termsOfServiceUrl("https://github.com/youkaisteve/autosrv")
+                .version("1.0")
+                .build();
+    }
+}
--- src/main/java/com/sm/autosrv/controller/HelloWorldController.java
+++ src/main/java/com/sm/autosrv/controller/HelloWorldController.java
@@ -1,16 +1,18 @@
 package com.sm.autosrv.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/greet")
 public class HelloWorldController {
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     public String index() {
         return "Hello World1";
     }
 
-    @RequestMapping("/bye")
+    @GetMapping("/bye")
     public String bye() {
         return "Bye";
     }
