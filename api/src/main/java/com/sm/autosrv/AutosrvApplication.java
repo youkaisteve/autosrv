2baf609f597a88fdbb1fcc83a966630fd4f74558
youkai <youkaisteve@gmail.com>
2017-08-30 09:14:06 +0800

    add banner

--- api/src/main/java/com/sm/autosrv/AutosrvApplication.java
+++ api/src/main/java/com/sm/autosrv/AutosrvApplication.java
@@ -5,18 +5,15 @@ import com.sm.autosrv.service.interfaces.dataservice.UserService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
+@ComponentScan
 public class AutosrvApplication {
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public UserService userService() {
-        return new UserServiceImpl();
     }
 
     public static void main(String[] args) {
--- data-service/src/main/java/com/sm/autosrv/data/serviceimpl/UserServiceImpl.java
+++ data-service/src/main/java/com/sm/autosrv/data/serviceimpl/UserServiceImpl.java
@@ -4,9 +4,11 @@ import com.sm.autosrv.data.dao.UserDao;
 import com.sm.autosrv.service.interfaces.dataservice.UserService;
 import com.sm.autosrv.service.interfaces.entity.ApplicationUser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 
+@Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
